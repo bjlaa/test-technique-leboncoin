7156818aaca9218d97f07797ed828fa609ac3b34
Benjamin <benjamin.j.arias@gmail.com>
2019-02-01 19:25:14 +0100

    feat: Created default files for Message component + default test

---
+++ src/components/Message/Message.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+describe('Message', () => {
+	it('should match its snapshot', () => {
+		expect(false).toBe(true);
+	});
+
+	it('should show the message passed via the message prop', () => {
+		expect(false).toBe(true);
+	});
+
+	it('should should show a div bearing the private id if the message is private', () => {
+		expect(false).toBe(true);
+	});
+});
