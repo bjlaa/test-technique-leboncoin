c76f31f2f6dbfd34697f76510b982e2187c60c87
Benjamin <benjamin.j.arias@gmail.com>
2019-02-01 21:33:44 +0100

    test: Added tests for Message

--- src/components/Message/Message.test.js
+++ src/components/Message/Message.test.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Message from './index';
+
+configure({ adapter: new Adapter() });
 
 describe('Message', () => {
+	const messageText = 'test';
+	const message = { id: 0, text: messageText };
+
 	it('should match its snapshot', () => {
-		expect(false).toBe(true);
+		const component = renderer.create(<Message />);
+
+		const tree = component.toJSON();
+
+		expect(tree).toMatchSnapshot();
 	});
 
 	it('should show the message passed via the message prop', () => {
-		expect(false).toBe(true);
+		const component = shallow(<Message message={message} />);
+
+		expect(component.find('span').text()).toEqual(messageText);
 	});
 
 	it('should should show a div bearing the private id if the message is private', () => {
-		expect(false).toBe(true);
+		message.private = true;
+
+		const component = shallow(<Message message={message} />);
+
+		expect(component.find('.message__private').length).toEqual(1);
 	});
 });
