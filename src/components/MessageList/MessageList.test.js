ee81d5e4f3641d01c7b2e686b010091c9488cfed
Benjamin <benjamin.j.arias@gmail.com>
2019-02-01 21:26:04 +0100

    test: Added tests for MessageList

--- src/components/MessageList/MessageList.test.js
+++ src/components/MessageList/MessageList.test.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+
+import MessageList from './index';
+import Message from '../Message';
+
+configure({ adapter: new Adapter() });
 
 describe('MessageList', () => {
   it('should match its snapshot', () => {
-    expect(false).toBe(true);
+  	const component = renderer.create(<MessageList />);
+
+  	const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 
   it('should render a <Message /> for each element in the messages array', () => {
-  	expect(false).toBe(true);
+  	const arrayMessages = [
+	  	{
+	  		id: 0,
+	  		text: 'test'
+	  	},
+			{
+	  		id: 1,
+	  		text: 'test'
+	  	},
+			{
+	  		id: 2,
+	  		text: 'test'
+	  	},
+  	];
+
+  	const component = mount(<MessageList messages={arrayMessages} />);
+
+  	expect(component.find(Message).length).toEqual(3);
   });
 });
